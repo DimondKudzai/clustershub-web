c99d8117f9e0a94cc240a59ad9404cf8d3f35b2b
dimondkudzai <diamondkudzai70@gmail.com>
2026-01-12 09:01:53 +0200

    updated

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///../tmp/clusters.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:////tmp/clusters.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     
