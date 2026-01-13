f573d926ae9331c7993ee9967fe5fa56f4665a20
dimondkudzai <diamondkudzai70@gmail.com>
2026-01-13 15:56:28 +0200

    backups

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 import os 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_M8IRz5fpyPSs@ep-silent-mud-ahoblu0u-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    SQLALCHEMY_DATABASE_URI = (
+        'postgresql://neondb_owner:npg_M8IRz5fpyPSs@ep-silent-mud-ahoblu0u-pooler.c-3.us-east-1.aws.neon.tech/neondb'
+        '?sslmode=require&channel_binding=require'
+    )
     SQLALCHEMY_ENGINE_OPTIONS = {
-    "pool_pre_ping": True,
-    "pool_recycle": 280,
-    "connect_args": {"connect_timeout": 10}
+        "pool_pre_ping": True,
+        "pool_recycle": 280,
+        "connect_args": {
+            "connect_timeout": 10,
+            "sslmode": "require"
+        }
     }
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "2409")  # optional fallback
     
 """
     MAIL_SERVER = os.getenv("MAIL_SERVER")
