10a936acf6b0bc323b73083ea31ec049d18539c2
dimondkudzai <diamondkudzai70@gmail.com>
2026-01-13 10:41:46 +0200

    backups

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp/clusters.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///./tmp/clusters.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     
