07d3146c565abd0671dd7b0338cfef5e061c7c92
dimondkudzai <diamondkudzai70@gmail.com>
2026-01-13 23:38:09 +0200

    backups

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os 
-
+class Config:
     SQLALCHEMY_DATABASE_URI = (
         'postgresql://neondb_owner:npg_M8IRz5fpyPSs@ep-silent-mud-ahoblu0u-pooler.c-3.us-east-1.aws.neon.tech/neondb'
         '?sslmode=require&channel_binding=require'
