2b39ddca860fead17a6cd7ca4963bf129cd02009
dimondkudzai <diamondkudzai70@gmail.com>
2026-01-13 22:44:38 +0200

    update

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os 
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///clusters.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "2409")
-
-"""
     SQLALCHEMY_DATABASE_URI = (
         'postgresql://neondb_owner:npg_M8IRz5fpyPSs@ep-silent-mud-ahoblu0u-pooler.c-3.us-east-1.aws.neon.tech/neondb'
         '?sslmode=require&channel_binding=require'
@@ -20,8 +14,15 @@ class Config:
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "2409")
-"""  
+    
+    
 """
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///clusters.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "2409")
+
+
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = os.getenv("MAIL_PORT")
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
