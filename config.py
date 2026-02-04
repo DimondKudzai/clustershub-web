3cd0561a4ef696f9b4c496e5ee182ac4b1c2d49c
dimondkudzai <diamondkudzai70@gmail.com>
2026-02-05 00:20:21 +0200

    updated

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-class Config:
+class Configfff:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///clusters.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "2409")
 
-class Configff:
+class Config:
     SQLALCHEMY_DATABASE_URI = (
         'postgresql://neondb_owner:npg_M8IRz5fpyPSs@ep-silent-mud-ahoblu0u-pooler.c-3.us-east-1.aws.neon.tech/neondb'
         '?sslmode=require&channel_binding=require'
