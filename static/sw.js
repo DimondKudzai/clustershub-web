973e488086455f8787fdef2c6443d8a538187d58
dimondkudzai <diamondkudzai70@gmail.com>
2026-02-08 22:42:59 +0200

    updated

--- static/sw.js
+++ static/sw.js
@@ -1,31 +1,27 @@
-const CACHE_NAME = 'clustersHub-v1';
+const CACHE_NAME = 'clustersHub-v21';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll([
+        '/',
         '/about',
         '/home',
         '/register',
+        '/login',
         '/static/createCluster.css',
         '/static/createCluster.js',
         '/static/banner.jpg',
         '/static/header.js',
         '/static/cluster.jpg',
-        '/static/logo.jpg',
-        '/user_requests',
-        '/clusters',
-        '/forgot',
-        '/myProfile',
-        '/terms',
-        '/login',
-        '/'
+        '/static/logo.jpg'
       ]))
       .catch((error) => console.error('Cache error:', error))
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  console.log('Fetching:', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
